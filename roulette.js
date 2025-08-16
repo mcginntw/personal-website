036236bcdf7693b5418f9d205ca0ec1fb8d9b5ac
mcginntw <mcginntw@mail.uc.edu>
2025-08-15 22:47:16 -0400

    Updated the roulette box and edited styles

--- roulette.js
+++ roulette.js
@@ -1,12 +1,35 @@
-const unicodeBox = document.getElementById('unicodeBox');
-
 const symbols = ["෴","⚐","⚑","⚜","★","☆","✦","✧","✩","✪","✫","✬","✭","✮","✯","✰","✱","✲","✳","✴","✵","✶","✷","✸","✹","✺","✻","✼","✽","☀︎","☁︎","☂︎","☃︎","☄︎","☼","☽","☾","✺","✹","⚒","⚔","⚖","⚗","⚙","⚓","⛏","⛓","⚕","✈︎","⛵︎","⛴︎","⛺︎","✂︎","✉︎","✍︎","✏︎","✒︎","✎","☎︎","☏","⌂","⌘","⌥","⌫","⌦","⎋","♪","♫","♩","♬","♭","♮","♯","♔","♕","♖","♗","♘","♙","♚","♛","♜","♝","♞","♟","♠","♣","♥","♦","♤","♧","♡","♢","✝","✚","✛","✜","✠","☥","☮","☯","⚘","✿","❀","❁","☘︎","⚡︎","⚠︎","♻︎","☣","☢","✇","✆",];
 
-let currentSymbol = 0;
 
-function updateSymbol() {
-    currentSymbol = (currentSymbol + 1) % symbols.length;
-    unicodeBox.textContent = symbols[currentSymbol];
-}
 
-setInterval(updateSymbol, 2000);
+(function InitSymbolBox() {
+    const canvas = document.getElementById('unicodeBox');
+    const ctx = canvas.getContext('2d');
+
+
+    function drawSymbol(glyph) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+
+        ctx.font = "bold 75px Nunito";
+        
+        ctx.fillStyle = '#F88379';
+        ctx.shadowColor = '#9a5852';
+        ctx.shadowBlur = 0;
+        ctx.shadowOffsetX = -2;
+        ctx.shadowOffsetY = -2;
+
+        ctx.fillText(glyph, canvas.width / 2, canvas.height / 2);
+    }
+
+    let currentSymbol = 0;
+
+    drawSymbol(symbols[currentSymbol]);
+
+    setInterval(() => {
+        currentSymbol = (currentSymbol + 1) % symbols.length;
+        drawSymbol(symbols[currentSymbol]);
+    }, 2000);
+    console.log('test');
+})();
